2f841ead8ae4627da1e6b8bde65880be367725e1
hotboyinside <rusov_pavel2002@mail.ru>
2024-11-30 14:19:31 +0300

    add: доработал конфиг eslint, добавил alias

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<header>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>News</li>
+					<li>Contacts</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
